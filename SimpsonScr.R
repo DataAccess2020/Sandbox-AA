6c7cdcfeee18d345b55d4a584626ecabfc4d238d
Antonio Alaia <alaia.comunicazione@gmail.com>
2022-02-10 16:04:34 +0100

    Second commit (after the exercise with for loop)

--- SimpsonScr.R
+++ SimpsonScr.R
@@ -1,2 +1,13 @@
 library(tidyverse)
 load("s3-simpsons.Rdata")
+
+str(simpsons[[1]])
+
+print(simpsons$""$title)
+
+simpsons[[1]]$title
+
+for(i in seq_along(simpsons)) {
+cat(simpsons[[i]]$title, "\n")
+}
+
